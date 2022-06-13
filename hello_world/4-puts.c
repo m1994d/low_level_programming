765c250d4716bdb16e3d17818711525b9c4a6703
m1994d <marlondiaz2994@gmail.com>
2022-06-13 17:51:25 -0500

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ *  * main - Prints "\"Programming is like building a multilingual puzzle"
+ *   * Return: 0
+ *    */
+
+#include <stdio.h>
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
