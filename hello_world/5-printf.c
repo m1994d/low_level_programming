de001e384c44a7ce675bcd50fcadd89265ab4002
m1994d <marlondiaz2994@gmail.com>
2022-06-13 17:52:19 -0500

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ *  * main - prints required sentence
+ *   * Return: 0
+ *    */
+#include <stdio.h>
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
