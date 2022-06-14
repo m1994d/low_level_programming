70f23aa282c4caa6c24349ef7a6f2eb2367a1384
m1994d <marlondiaz2994@gmail.com>
2022-06-14 11:46:49 -0500

    101-print_comb4.c

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numberRight;
+	int numberLeft;
+	int numberMiddle;
+
+		for (numberLeft = 48; numberLeft <= 57; numberLeft++)
+		{
+			for (numberMiddle = numberLeft + 1; numberMiddle <= 57; numberMiddle++)
+			{
+
+				for (numberRight = numberMiddle + 1; numberRight <= 57; numberRight++)
+																			{
+																				if (numberMiddle != numberRight)
+																				{
+	
+																					putchar(numberLeft);
+						putchar(numberMiddle);
+						purchar(numberRight);													if (!(
+							numberRight ==  57
+							&& numberMiddle == 56
+							&& numberLeft == 55
+						))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+		putchar('\n');
+		eturn (0);
+}
