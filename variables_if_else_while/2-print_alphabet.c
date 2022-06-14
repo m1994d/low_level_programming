b5d4ebc4bcb1c3d2a3eea5648d5e7fd553febe11
m1994d <marlondiaz2994@gmail.com>
2022-06-14 10:33:06 -0500

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Prints lowercase alphabet
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 123)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
