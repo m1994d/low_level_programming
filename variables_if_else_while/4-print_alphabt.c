9e578ad7bf376f746b94a9ba826c346edf4d44c0
m1994d <marlondiaz2994@gmail.com>
2022-06-14 11:14:56 -0500

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/**
+ * main - Prints lowercase alphabet
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	for (c = 97; c < 123; c++)
+	{
+		if (c == 101)
+		{
+		}
+		else if (c == 113)
+		{
+		}
+		else
+		{
+
+			putchar(c);
+																	}
+	}
+	putchar('\n');
+	return (0);
+}
