148435a33e87c67b6ae9ed5947ef63b47622c8ad
m1994d <marlondiaz2994@gmail.com>
2022-06-14 11:33:04 -0500

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numberRight;
+	int numberLeft;
+	int leftLimit = 48;
+
+	for (numberLeft = 48; numberLeft <= 57; numberLeft++)
+	{
+		for (numberRight = leftLimit++; numberRight <= 57; numberRight++)
+		{
+		
+			if (numberRight != numberLeft)
+			{
+			
+				putchar(numberLeft);
+				putchar(numberRight);
+				if (!(numberRight ==  57 && numberLeft == 56))
+				{
+				
+					putchar(',');
+																				putchar(' ');
+																																		}
+																		}
+		
+		}
+	}
+	putchar('\n');
+	return (0);
+}
