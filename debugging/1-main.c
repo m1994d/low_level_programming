cf877e161620672c85d218ee3cb75763c0d717e1
m1994d <marlondiaz2994@gmail.com>
2022-06-16 14:49:19 -0500

    1-main.c

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 	/*
  	*while (i < 10)
