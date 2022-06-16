dc1854b1844ab6f11dff0a5af06388cc49159250
m1994d <marlondiaz2994@gmail.com>
2022-06-16 13:19:58 -0500

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers and returns the result.
+ * @a: First number to add
+ * @b: second number to add
+ * Return: a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
