dd05a2d6049207fca63baac523b4f074d10bc49e
m1994d <marlondiaz2994@gmail.com>
2022-06-22 10:28:39 -0500

    10-print_triangle.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
