f3d102e57808932bcc037b7cb0c2ef7724da630f
m1994d <marlondiaz2994@gmail.com>
2022-06-28 12:37:23 -0500

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer to string to measure
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (*(s + i) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * Return: Pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lendest;
+	int lensrc;
+	int lenfdest;
+	int i;
+
+	lendest = _strlen(dest);
+	lensrc = _strlen(src);
+	lenfdest = lendest + lensrc;
+	i = 0;
+	while (lendest <= lenfdest)
+	{
+		*(dest + lendest) = *(src + i);
+		lendest++;
+		i++;
+	}
+	return (dest);
+}
