7e288b7bc3eacc0244a6dc8ee1c665098b8bcade
m1994d <marlondiaz2994@gmail.com>
2022-06-28 12:23:53 -0500

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer to string to measure
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (*(s + i) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest.
+ * @dest: Pointer to destiny
+ * @src: Pointer to source
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = _strlen(src);
+	for (i = 0; i <= len; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
