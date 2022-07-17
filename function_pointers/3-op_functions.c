6c008fed1ea3b350128a1dba2989fba86220b4c3
m1994d <marlondiaz2994@gmail.com>
2022-07-16 19:21:25 -0500

    function_pointers

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - returns the sum of a and b
+ * @a: First number to add
+ * @b: Second number to add
+ * Return: Sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: First number
+ * @b: Second number
+ * Return: Difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b.
+ * @a: First number
+ * @b: Second number
+ * Return: Product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b.
+ * @a: Fist number
+ * @b: Second number
+ * Return: Division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: First number
+ * @b: Second number
+ * Return: remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
