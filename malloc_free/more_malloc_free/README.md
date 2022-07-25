de16f93edf729875aa856dbffa2bdb7ed7e69d5d
m1994d <marlondiaz2994@gmail.com>
2022-07-25 13:27:45 -0500

    Nothing specified, nothing added. hint: Maybe you wanted to say 'git add .'? hint: Turn this message off by running hint: "git config advice.addEmptyPathspec false"

--- malloc_free/more_malloc_free/README.md
+++
@@ -1,8 +0,0 @@
-# More_Malloc_Free
-
-0-malloc_checked.c - allocates memory using malloc.
-1-string_nconcat.c - concatenates two strings.
-2-calloc.c - allocates memory for an array, using malloc.
-3-array_range.c - creates an array of integers.
-
-* Made by Marlon Andres Diaz for Holberton School Col.
--- static_libraries/2-strncpy.c
+++ static_libraries/2-strncpy.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _strlen - function that returns the length of a string.
+ * _strncpy - function that copies a string.
  *
- * @s: input parameter for the function
- * Return: void.
+ * @dest: input parameter
+ * @n: input parameter
+ * @src: input parameter
+ * Return: pointer to the resulting string dest.
  */
 
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	
+	return (dest);
 }
