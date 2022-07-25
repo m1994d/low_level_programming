76227c4111c478f6069db6e910228b3cca24ed9c
m1994d <marlondiaz2994@gmail.com>
2022-07-25 13:04:59 -0500

    free_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/*
+ * free_list - frees a list_t list.
+ * @head: pointer to head of linked list
+ */
+
+void free_list(list_t *head)
+{
+	if (head && head->next)
+		free_list(head->next);
+	if (head && head->str)
+		free(head->str);
+	if (head)
+	free(head);
+}
