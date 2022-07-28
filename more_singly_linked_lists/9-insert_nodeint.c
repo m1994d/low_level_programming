b2049d9a9df95d572e8a35fd95870567fd5eaccf
m1994d <marlondiaz2994@gmail.com>
2022-07-28 14:30:37 -0500

    insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,70 +1,53 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * add_node - Entry point
- * @node_to_set: current node
- * @n: value
- * @node: index
- * Return: Always 0 (Success)
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: Pointer to list's head pointer
+ * @idx: index of the list where the new node should be added
+ * @n: intiger to add
+ * Return: Address of new node, or NULL in fail
  */
-listint_t *add_node(listint_t **node_to_set, listint_t *node, int n)
-{
-	listint_t *new_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = node;
-	*node_to_set = new_node;
-
-	return (*node_to_set);
-
-}
-/**
- * insert_node_at_index - Entry point
- * @node: current node
- * @n: value
- * @idx: value
- * @before: index
- * Return: Always 0 (Success)
- */
-listint_t *insert_node_at_index(
-	listint_t *node,
-	listint_t *before,
-	unsigned int idx,
-	int n
-)
-{
-
-	if (before == NULL)
-		return (NULL);
-
-	if (idx == 0)
-		return (add_node(&before->next, node, n));
-
-	return (insert_node_at_index(node->next, before->next, idx - 1, n));
-}
-
-
-/**
- * insert_nodeint_at_index - Entry point
- * @head: current node
- * @n: value
- * @idx: index
- * Return: Always 0 (Success)
- */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	if (head == NULL)
+	unsigned int position;
+	listint_t *aux, *ret;
+
+	position = 0;
+	if (head)
+		aux = *head;
+	ret = (listint_t *)malloc(sizeof(listint_t));
+	if (ret == NULL)
 		return (NULL);
-
-	if (idx == 1)
-		return (add_node(head, *head, n));
-
-	if (*head == NULL)
-		return (NULL);
-
-	return (insert_node_at_index((*head)->next, *head, idx - 1, n));
+	ret->n = n;
+	while (aux && aux->next)
+	{
+		if (idx == 0)
+		{
+			ret->next = aux;
+			(*head) = ret;
+			return (ret);
+		}
+		else if ((position + 1) == idx)
+		{
+			ret->next = aux->next;
+			aux->next = ret;
+			return (ret);
+		}
+		position += 1;
+		aux = aux->next;
+	}
+	if (head && aux == NULL && idx == 0)
+	{
+		ret->next = NULL;
+		(*head) = ret;
+		return (ret);
+	}
+	else if ((position + 1) == idx)
+	{
+		aux->next = ret;
+		ret->next = NULL;
+		return (ret);
+	}
+	return (NULL);
 }
