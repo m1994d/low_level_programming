3df098b51d7376651b9bd4b0e4b22c1768e7cf78
m1994d <marlondiaz2994@gmail.com>
2022-07-28 14:40:02 -0500

    lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,21 +1,23 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- * 
- * Description: singly linked list node structure
- * for Holberton project
- */
+ *  * struct listint_s - singly linked list
+ *   * @n: integer
+ *    * @next: points to the next node
+ *     *
+ *      * Description: singly linked list node structure
+ *       *
+ *        */
+
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+		int n;
+			struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -30,8 +32,10 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+unsigned int distance_nodes(const listint_t *n1, const listint_t *n2);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
+unsigned int distance_nodes2(listint_t *n1, listint_t *n2);
+size_t free_listint_safe(listint_t **head);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LIST_H */
