a179732e928e6aff783453b9c219697e455ade94
m1994d <marlondiaz2994@gmail.com>
2022-07-28 12:41:22 -0500

    free_listint

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint - Entry point
+ * @head: current node
+ * Return: Always 0 (Success)
+ * */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	free_listint(head->next);
+	free(head);
+}
