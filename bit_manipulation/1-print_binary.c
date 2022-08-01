c6a05e00746ba2da59c03c15c4984d2c059102c0
m1994d <marlondiaz2994@gmail.com>
2022-08-01 10:23:38 -0500

    print_binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  * print_binary - prints the binary representation of a number.
+ *   * @n: Number to print in binary
+ *    */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int aux;
+	int power;
+
+	aux = n;
+	power = 0;
+	if (n == 0)
+		_putchar('0');
+	while (aux > 0)
+	{
+		power++;
+		aux = aux >> 1;
+	}
+	while (power > 0)
+	{
+		aux = n >> (power - 1);
+		_putchar((aux & 1) + '0');
+		aux = n;
+		power--;
+	}
+}
